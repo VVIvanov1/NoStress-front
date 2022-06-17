90bcbb3d8a32bb7966f490d4093a1debb322829d
VVIvanov1 <ivanovv.tca@gmail.com>
2022-06-17 20:06:28 +0600

    changed my orders url

--- src/api/useGetMyOrders.js
+++ src/api/useGetMyOrders.js
@@ -5,9 +5,9 @@ import { useEffect, useState } from "react";
 var BASE_URL = "";
 
 if (window.location.origin === "http://localhost:3000") {
-  BASE_URL = "http://localhost:5000/orders";
+  BASE_URL = "http://localhost:5000/orders/get-my-orders";
 } else if (window.location.origin === "https://corp-baigroupkz.netlify.app") {
-  BASE_URL = "https://backend-baigroupkz.herokuapp.com/orders";
+  BASE_URL = "https://backend-baigroupkz.herokuapp.com/orders/get-my-orders";
 }
 
 function useFetchMyOrders() {
@@ -17,7 +17,7 @@ function useFetchMyOrders() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const ords = await axios.get(`${BASE_URL}?user=${auth.email}`, {
+        const ords = await axios.get(`${BASE_URL}`, {
           withCredentials: true,
         });
 
